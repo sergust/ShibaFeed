cb53e332d7ea2c0850f34c3da7bdbb694a41c649
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-13 21:12:45 +0300

    Implemet fluid header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
-  <b-container>
-    <b-row align-h="between" align-v="center" class="header">
-      <b-col class="header--logo-container" cols="2">
-        <b-img src="../assets/logo.svg" class="header--logo mx-auto mt-3 mb-3"></b-img>
-      </b-col>
-      <b-col cols="3" class="header--auth">
-        <p>🙌 Hi, username!</p>
-      </b-col>
-    </b-row>
+  <b-container fluid class="header">
+    <b-container>
+      <b-row align-h="between" align-v="center">
+        <b-col class="header--logo-container" cols="2">
+          <b-img src="../assets/logo.svg" class="header--logo mx-auto mt-3 mb-3"></b-img>
+        </b-col>
+        <b-col cols="3" class="header--auth">
+          <p>🙌 Hi, username!</p>
+        </b-col>
+      </b-row>
+    </b-container>
   </b-container>
 </template>
 
@@ -19,8 +21,6 @@
 
 .header {
   background-color: #756c83;
-  border-bottom-left-radius: 15px;
-  border-bottom-right-radius: 15px;
   box-shadow: 0px 0px 10px grey;
 }
 
