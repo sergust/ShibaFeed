63b5c59f803c651623fed3ea10694c0ddc829cfd
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-14 11:28:51 +0300

    Add Post layout

--- client/src/components/Feed/Post.vue
+++ client/src/components/Feed/Post.vue
@@ -7,21 +7,52 @@
     class="mb-5"
   >
     <b-card-text>
-      This is a wider card with supporting text below as a natural lead-in to additional content.
-      This card has even longer content than the first to show that equal height action.
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This card has even longer content than the first to
+      show that equal height action.
     </b-card-text>
     <template v-slot:footer>
       <b-container>
         <b-row align-v="center" no-gutters>
-          <b-col cols="2" md="2" sm="2" xl="1">
-            <div class="post--footer-block">ava</div>
+          <b-col>
+            <div>
+              <p>
+                <font-awesome-icon
+                  :icon="['fas', 'user-ninja']"
+                  class="mr-3"
+                /><small class="text-muted">User Name</small>
+              </p>
+            </div>
+            <div>
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
           </b-col>
-          <b-col cols="10" md="2" sm="2" xl="10">
-            <small class="text-muted">User Name</small>
-            <br />
-            <small class="text-muted">Last updated 3 mins ago</small>
+          <b-col class="post--comment-block">
+            <a class="post--comment-icon mt-2 mr-4">
+              <font-awesome-icon :icon="['fas', 'comment']" class="mr-2" />3
+            </a>
+            <a
+              ><a class="post--comment-icon mt-2">
+                <font-awesome-icon :icon="['fas', 'paw']" class="mr-2" />3
+              </a></a
+            >
           </b-col>
-          <b-col></b-col>
+          <!-- <b-col cols="6" md="6" sm="" xl="6">
+            <div class="post--footer-block">
+              <p>
+                <font-awesome-icon :icon="['fas', 'user-ninja']" />
+                <small class="text-muted">User Name</small>
+                <br />
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </b-col>
+          <b-col cols="auto" md="2" sm="2" xl="3"> </b-col>
+          <b-col offset="6" cols="1">
+            <p class="post--comment-icon">
+              <font-awesome-icon :icon="['fas', 'comment']" class="mr-3" />3
+            </p>
+          </b-col> -->
         </b-row>
       </b-container>
     </template>
@@ -35,5 +66,13 @@ export default {};
 <style>
 .post--footer-block {
   display: inline-block;
+}
+
+.post--comment-block {
+  text-align: right;
+}
+
+.post--comment-icon:hover {
+  cursor: pointer;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,29 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faComment,
+  faUserNinja,
+  faPaw
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+library.add(faComment, faUserNinja, faPaw);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
