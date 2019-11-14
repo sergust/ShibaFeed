386b216c074b00f47217b8aacc168b5dc91c2e5f
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-14 12:26:26 +0300

    Server init

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+
+// Confing env variables
+dotenv.config({ path: './config/config.env' });
+
+// Body parser
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: 'Kek lol'
+  });
+});
+
+const PORT = process.env.PORT | 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
