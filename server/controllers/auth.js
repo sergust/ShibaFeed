19bc2b61fa4c0ca4ed5b5f2be100fec0db8b978a
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-15 15:55:59 +0300

    Implement token and cookie responer

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,11 +51,30 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse('Invalid credentials', 400));
   }
 
+  sendTokenResponse(user, 200, res);
+});
+
+// Respond with token and cookie
+const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
-  res.status(200).json({
-    success: true,
-    token
-  });
-});
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie('token', token, options)
+    .json({
+      success: true,
+      token
+    });
+};
