d25ab135a0c14d2295b602b7d0fa2265a5339c00
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-15 15:30:04 +0300

    Implement user login

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const slugify = require('slugify');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
 
 const UserSchema = new mongoose.Schema({
   firstName: {
@@ -59,5 +62,17 @@ UserSchema.pre('save', async function(next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
