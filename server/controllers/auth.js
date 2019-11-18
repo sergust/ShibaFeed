6a3ae1731e95478a1b1d1e3099f78dbb44c4524e
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-18 10:45:00 +0300

    Implement sending email for Forgot Password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require('crypto');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc    Register user
@@ -87,10 +88,39 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save({ validateBeforeSave: false });
 
-  res.status(200).json({
-    success: true,
-    data: user
-  });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receivng this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message
+    });
+
+    res.status(200).json({
+      succes: true,
+      message: 'Email sent',
+      data: user
+    });
+  } catch (e) {
+    console.log(e);
+    user.getResetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
+
+  // res.status(200).json({
+  //   success: true,
+  //   data: user
+  // });
 });
 
 // Respond with token and cookie
