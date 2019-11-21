758db3681abdc2c119a4ca33e8ca67ea902be156
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-21 17:53:59 +0300

    feat: add UI comment changer

--- client/src/components/Feed/PostComment.vue
+++ client/src/components/Feed/PostComment.vue
@@ -1,20 +1,67 @@
 <template>
   <div>
-    <p>
-      <font-awesome-icon :icon="['fas', 'user-ninja']" class="mr-3" />
-      <small class="text-muted">{{ fullName() }} said: &nbsp;</small>
-      <small class="text-muted">{{ commentBody }}</small>
-    </p>
+    <div class="post--comment" v-if="!isEditing">
+      <p class="post--comment--p">
+        <font-awesome-icon :icon="['fas', 'user-ninja']" class="mr-3" />
+        <small class="text-muted">{{ fullName() }} said: &nbsp;</small>
+        <small class="text-muted">{{ commentBody }}</small>
+        <font-awesome-icon
+          :icon="['fas', 'edit']"
+          class="post-comment--delete-icon ml-2"
+          @focus="showEdit"
+        />
+      </p>
+    </div>
+    <div v-if="isEditing">
+      <b-input-group
+        :prepend="fullName()"
+        class="mt-3 mb-3"
+        @keydown.esc="hideEdit"
+        @focusout="hideEdit"
+      >
+        <b-form-input
+          ref="inputItem"
+          :value="this.$props.commentBody"
+        ></b-form-input>
+        <b-input-group-append>
+          <b-button variant="info"
+            ><font-awesome-icon :icon="['fas', 'edit']"
+          /></b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      isEditing: false
+    };
+  },
   methods: {
     fullName() {
       return `${this.$props.commentOwner.firstName} ${this.$props.commentOwner.lastName}`;
+    },
+    showEdit() {
+      this.isEditing = true;
+      this.$nextTick(() => this.$refs.inputItem.$el.focus());
+    },
+    hideEdit() {
+      this.isEditing = false;
     }
   },
   props: ['commentBody', 'commentOwner']
 };
 </script>
+
+<style scoped>
+.post-comment--delete-icon {
+  display: none;
+}
+
+.post--comment--p:hover .post-comment--delete-icon {
+  display: initial;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -10,15 +10,30 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
   faComment,
   faUserNinja,
-  faPaw
+  faPaw,
+  faEdit
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faComment, faUserNinja, faPaw);
+library.add(faComment, faUserNinja, faPaw, faEdit);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(require('vue-moment'));
+
+Vue.directive('click-outside', {
+  bind: function(el, binding, vnode) {
+    el.clickOutsideEvent = function(event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent);
+  },
+  unbind: function(el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent);
+  }
+});
 
 Vue.config.productionTip = false;
 
