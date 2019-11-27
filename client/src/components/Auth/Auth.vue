b29c4152afaff4509605d2fc7a379ee17338fbe6
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-27 17:15:02 +0300

    feat: add authorization verification

--- client/src/components/Auth/Auth.vue
+++ client/src/components/Auth/Auth.vue
@@ -3,22 +3,69 @@
     <b-col cols="11" md="6" sm="8" xl="4">
       <div class="auth--login mb-5">
         <h4 class="mx-auto">Log in</h4>
-        <b-form-input type="email" placeholder="Your email" v-model="loginEmail"></b-form-input>
-        <b-form-input type="password" placeholder="Your password" v-model="loginPassword"></b-form-input>
+        <div>
+          <b-form-input
+            type="email"
+            placeholder="Your email"
+            @blur="$v.loginEmail.$touch()"
+            v-model="loginEmail"
+            :state="!$v.loginEmail.$error"
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            Please provide a valid email address
+          </b-form-invalid-feedback>
+        </div>
+
+        <div>
+          <b-form-input
+            type="password"
+            placeholder="Your password"
+            @blur="$v.loginPassword.$touch()"
+            v-model="loginPassword"
+            :state="!$v.loginPassword.$error"
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            Your password should be at least 6 characters
+          </b-form-invalid-feedback>
+        </div>
+
         <p>
-          <b-button @click="logIn" variant="outline-primary" class="mr-4">Log in</b-button>
-          <b-link v-b-modal.modal-forgot-password to="/forgotpassword">Forgot password?</b-link>
+          <b-button @click="logIn" variant="outline-primary" class="mr-4"
+            >Log in</b-button
+          >
+          <b-link v-b-modal.modal-forgot-password to="/forgotpassword"
+            >Forgot password?</b-link
+          >
         </p>
       </div>
       <div>
         <div class="auth--signup">
           <h4>First time here?</h4>
           <p>Sign up for ShibaFeed</p>
-          <b-form-input type="text" placeholder="Your first name" v-model="signUpFirstName"></b-form-input>
-          <b-form-input i type="text" placeholder="Your last name" v-model="signUpLastName"></b-form-input>
-          <b-form-input type="email" placeholder="Your email" v-model="signUpEmail"></b-form-input>
-          <b-form-input type="password" placeholder="Your password" v-model="signUpPassword"></b-form-input>
-          <b-button @click="signUp" block type="success" variant="success">Sign Me Up!</b-button>
+          <b-form-input
+            type="text"
+            placeholder="Your first name"
+            v-model="signUpFirstName"
+          ></b-form-input>
+          <b-form-input
+            i
+            type="text"
+            placeholder="Your last name"
+            v-model="signUpLastName"
+          ></b-form-input>
+          <b-form-input
+            type="email"
+            placeholder="Your email"
+            v-model="signUpEmail"
+          ></b-form-input>
+          <b-form-input
+            type="password"
+            placeholder="Your password"
+            v-model="signUpPassword"
+          ></b-form-input>
+          <b-button @click="signUp" block type="success" variant="success"
+            >Sign Me Up!</b-button
+          >
         </div>
       </div>
     </b-col>
@@ -26,6 +73,7 @@
 </template>
 
 <script>
+import { required, email, minLength } from 'vuelidate/lib/validators';
 import makeToast from '../../utils/makeToast';
 export default {
   data() {
@@ -58,6 +106,16 @@ export default {
       this.$store.dispatch('signup', { vm: this, reqSignUpBody });
     },
     ...makeToast
+  },
+  validations: {
+    loginEmail: {
+      required,
+      email
+    },
+    loginPassword: {
+      required,
+      minLen: minLength(6)
+    }
   }
 };
 </script>
@@ -79,5 +137,10 @@ export default {
 .auth--signup > h4,
 .auth--signup > p {
   text-align: center;
+}
+
+.input.invalid input {
+  color: red;
+  border: 1px solid red;
 }
 </style>
