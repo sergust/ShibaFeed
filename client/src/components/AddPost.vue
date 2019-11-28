e07e62c9f908d75789c17cbdfb7bd19b068edd20
Sergey Ustinov <ustinov.biz@gmail.com>
2019-11-28 11:18:33 +0300

    feat: add post verification

--- client/src/components/AddPost.vue
+++ client/src/components/AddPost.vue
@@ -2,23 +2,48 @@
   <b-container v-if="authenticated">
     <b-row class="mb-4 ml-1 mr-1" align-h="center">
       <b-col cols="12" xl="8" lg="8" md="12" sm="12" class="add-post--block">
-        <b-form-input
-          id="input-large"
-          size="lg"
-          placeholder="What's new?"
-          class="mb-2 mt-2"
-          v-model="title"
-        ></b-form-input>
+        <div>
+          <b-form-input
+            id="input-large"
+            size="lg"
+            placeholder="What's new?"
+            class="mb-2 mt-2"
+            @blur="$v.title.$touch()"
+            v-model="title"
+            :state="$v.title.$dirty ? !$v.title.$error : null"
+          ></b-form-input>
+          <b-form-invalid-feedback class="ml-3">
+            Please enter a title
+          </b-form-invalid-feedback>
+        </div>
+
         <hr />
-        <b-form-textarea
-          id="textarea-rows"
-          placeholder="Tell a bit more :)"
-          rows="6"
-          v-model="description"
-        ></b-form-textarea>
+        <div>
+          <b-form-textarea
+            id="textarea-rows"
+            placeholder="Tell a bit more :)"
+            rows="6"
+            @blur="$v.description.$touch()"
+            v-model="description"
+            :state="$v.description.$dirty ? !$v.description.$error : null"
+          ></b-form-textarea>
+          <b-form-invalid-feedback class="ml-3 mt-3 mb-3">
+            Description should be less than
+            {{ $v.description.$params.maxLen.max }} characters and should not be
+            empty
+          </b-form-invalid-feedback>
+        </div>
+
         <div class="mb-3">
-          <b-button v-show="false" variant="outline-primary" class="mr-3">Add a photo</b-button>
-          <b-button variant="success" @click="sendPost">Post!</b-button>
+          <b-button v-show="false" variant="outline-primary" class="mr-3"
+            >Add a photo</b-button
+          >
+          <b-button
+            variant="success"
+            @click="sendPost"
+            :disabled="$v.$anyDirty && !$v.$anyError ? false : true"
+            >Post!</b-button
+          >
         </div>
       </b-col>
     </b-row>
@@ -26,6 +51,7 @@
 </template>
 
 <script>
+import { required, minLength, maxLength } from 'vuelidate/lib/validators';
 export default {
   data() {
     return {
@@ -48,6 +74,18 @@ export default {
   computed: {
     authenticated() {
       return this.$store.getters.isAuthenticated;
+    }
+  },
+  validations: {
+    title: {
+      required,
+      minLen: minLength(1),
+      maxLen: maxLength(50)
+    },
+    description: {
+      required,
+      minLen: minLength(1),
+      maxLen: maxLength(500)
     }
   }
 };
